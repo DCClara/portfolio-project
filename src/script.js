a236a7427d6b82f96cea52e780fb33e881099fa0
DCClara <141446800+DCClara@users.noreply.github.com>
2023-12-30 13:45:18 -0500

    added skills to the about me section

--- src/script.js
+++ src/script.js
@@ -21,56 +21,6 @@ function nextSlide() {
   showSlide(currentIndex + 1);
 }
 
-//icon grid functions
-const skills = [
-  { name: "HTML", icon: "src/images/html-icon.png" },
-  { name: "CSS", icon: "src/images/css-icon.png" },
-  { name: "JavaScript", icon: "src/images/javascript-icon.png" },
-  { name: "Bootstrap", icon: "src/images/bootstrap-icon.png" },
-  { name: "VS Code Editor", icon: "src/images/editor-icon.png" },
-  { name: "API", icon: "src/images/api-icon.png" },
-  { name: "GitHub", icon: "src/images/github-icon.png" },
-  { name: "Netlify Hosting", icon: "src/images/hosting-icon.png" },
-  { name: "Responsive Design", icon: "src/images/responsive-icon.png" },
-  { name: "Flexbox", icon: "src/images/flexbox-icon.png" },
-  { name: "SEO", icon: "src/images/seo-icon.png" },
-  { name: "React", icon: "src/images/React-icon.png" },
-  { name: "AI", icon: "src/images/ai-icon.png" },
-];
-
-const columns = 12;
-const rows = Math.ceil(skills.length / columns);
-const skillsContainer = document.getElementById("skillsContainer");
-
-for (let i = 0; i < rows; i++) {
-  const rowDiv = document.createElement("div");
-  rowDiv.classList.add("row");
-
-  for (let j = 0; j < columns; j++) {
-    const skillIndex = i * columns + j;
-    if (skillIndex < skills.length) {
-      const skill = skills[skillIndex];
-
-      const colDiv = document.createElement("div");
-      colDiv.classList.add("col-lg-3");
-
-      const skillIconDiv = document.createElement("div");
-      skillIconDiv.classList.add("skill-icon");
-
-      const img = document.createElement("img");
-      img.src = skill.icon;
-      img.alt = skill.name;
-      img.classList.add("img-fluid", "mx-auto", "d-block", "rounded");
-
-      skillIconDiv.appendChild(img);
-      colDiv.appendChild(skillIconDiv);
-      rowDiv.appendChild(colDiv);
-    }
-  }
-
-  skillsContainer.appendChild(rowDiv);
-}
-
 //Show more button functions
 document.getElementById("toggleButton").addEventListener("click", function () {
   var row2 = document.getElementById("row2");
